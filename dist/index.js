55e0bd19ea3f373060830902025c5a9a2eb72e4a
Mark Valentino <mike9377mike9377@gmail.com>
2024-02-22 02:26:24 -0500

    hopefully got exports working right

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Blink_1 = require("./components/Blink");
+Object.defineProperty(exports, "Blink", { enumerable: true, get: function () { return Blink_1.default; } });
+var Center_1 = require("./components/Center");
+Object.defineProperty(exports, "Center", { enumerable: true, get: function () { return Center_1.default; } });
+var Eighty8x31_1 = require("./components/Eighty8x31");
+Object.defineProperty(exports, "Eighty8x31", { enumerable: true, get: function () { return Eighty8x31_1.default; } });
+var Marquee_1 = require("./components/Marquee");
+Object.defineProperty(exports, "Marquee", { enumerable: true, get: function () { return Marquee_1.default; } });
+
