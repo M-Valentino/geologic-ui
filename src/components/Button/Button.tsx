00298dba146e0e057c9fb52dd6d94482d0578515
Mark Valentino <mike9377mike9377@gmail.com>
2024-02-22 13:56:38 -0500

    made 90's style button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import "./Button.scss";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <button style={{ border: "2px outset #eee #777 #777 #eee", backgroundColor: "#ccc" }}>
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
