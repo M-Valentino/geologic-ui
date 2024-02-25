519ede2e46de8259f9f2f1e9a895e182bdfbcefc
Mark Valentino <mike9377mike9377@gmail.com>
2024-02-25 17:22:58 -0500

    added working icon component

---
+++ src/components/Icon/Icon.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Icon from './Icon';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'ReactComponentLibrary/Icon',
+  component: Icon,
+} as ComponentMeta<typeof Icon>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
+
+export const Beer = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Beer.args = {
+  name: "beer",
+};
+
---
+++ src/components/Icon/Icon.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Icon from "./Icon";
+
+describe("Icon", () => {
+  test("renders the Icon component", () => {
+    render(<Icon name="alien"/>);
+  });
+});
