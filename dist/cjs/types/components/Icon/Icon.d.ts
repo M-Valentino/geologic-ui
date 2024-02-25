267478ee44cac2aa80aa1886cdbe07af88d633a8
Mark Valentino <mike9377mike9377@gmail.com>
2024-02-25 17:36:09 -0500

    changed version and made rollup

---
+++ dist/cjs/types/components/Icon/Icon.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+export interface IconProps {
+    name: string;
+    size?: string;
+}
+declare const Icon: (props: IconProps) => JSX.Element;
+export default Icon;
---
+++ dist/cjs/types/components/Icon/Icon.stories.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+declare const _default: ComponentMeta<(props: import("./Icon").IconProps) => JSX.Element>;
+export default _default;
+export declare const BeerNoSizeArg: ComponentStory<(props: import("./Icon").IconProps) => JSX.Element>;
+export declare const AlienSM: ComponentStory<(props: import("./Icon").IconProps) => JSX.Element>;
+export declare const TelephoneMD: ComponentStory<(props: import("./Icon").IconProps) => JSX.Element>;
+export declare const FolderLG: ComponentStory<(props: import("./Icon").IconProps) => JSX.Element>;
+export declare const HeartXL: ComponentStory<(props: import("./Icon").IconProps) => JSX.Element>;
